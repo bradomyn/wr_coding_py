04c3a733c6d5b8ff8bee812e458c57fba1509182
C.Prados <c.prados@gsi.de>
2015-04-15 22:24:49 +0200

    fec: add config parameters

--- fec.py
+++ fec.py
@@ -3,25 +3,46 @@ sys.path.append('./rs/')
 
 from rs_code import rs_code
 
+class fec_cfg:
+    op = [[] for l in range(0, 4) ]
+
+    def __init__(self):
+        fec_cfg.op[0] = {'pkt': 2, 'n': 256, 'k' : 128}
+        fec_cfg.op[1] = {'pkt': 3, 'n': 252, 'k' : 84}
+        fec_cfg.op[2] = {'pkt': 4, 'n': 256, 'k' : 128}
+        fec_cfg.op[3] = {'pkt': 4, 'n': 256, 'k' : 64}
+
+
 class fec_code:
     pkt = 4
     TRYING = 1
     FAIL = 2
     OK = 3
+    ID_IDX = 1
+    SYM_IDX = 3
     dec_msg = []
+    cfg = fec_cfg()
 
-    def __init__(self, code_id):
-        fec_code.pkt = 4
-        self.code_id = 1 # RS = 1 LDPC = 2 LT = 3
-        self.pkt_id = 0
-        self.block_symbol = 128
-        self.pkt_ln = 512
-        self.block_ln = 1024
+    def __init__(self, code_id, fec_cfg, block_ln):
+
+        cfg =  fec_code.cfg.op[code_id - 1]
+
+        self.ratio = cfg['n'] / cfg['k']
+        self.alpha = cfg['pkt'] / self.ratio # number of packets with src symbols
+        self.beta =  cfg['pkt'] - self.ratio # number of packts with enc symbols
+        self.code_id = code_id
+        fec_code.pkt = cfg['pkt']
+        self.block_symbol = cfg['k']
+        self.pkt_ln = block_ln / self.alpha
+        self.code_iter = self.pkt_ln / self.block_symbol
+        self.block_ln = block_ln
+
         fec_code.dec_msg = [0] * self.block_ln
+        self.pkt_id = 0
         self.recv_msg = 0
 
         if code_id == 1:
-            self.code = rs_code()
+            self.code = rs_code(cfg['n'], cfg['k'])
 
     def get_data(self, msg_file):
 
@@ -36,43 +57,44 @@ class fec_code:
     def encode(self, msg):
         fec_msg = [[] for l in range(0, self.pkt) ]
 
-        for i in range (0,self.pkt/2 ):
-            enc_msg = self.code.encode(msg[i * self.pkt_ln : self.pkt_ln * (i + 1)])
-
-            j = i + 2
+        # FEC header
+        for i in range(0, self.pkt):
             fec_msg[i].append(self.code_id)
-            self.pkt_id = i
-            fec_msg[i].append(self.pkt_id)
+            fec_msg[i].append(i)
             b_symbol = self.block_symbol * i
             fec_msg[i].append(b_symbol)
-            fec_msg[i].append(enc_msg[0 : self.pkt_ln/2])
 
-            fec_msg[j].append(self.code_id)
-            self.pkt_id = j
-            fec_msg[j].append(self.pkt_id)
-            b_symbol = self.block_symbol * j
-            fec_msg[j].append(b_symbol)
-            fec_msg[j].append(enc_msg[self.pkt_ln/2: ])
+        # FEC payload
+        slice_pkt = self.pkt_ln / self.block_symbol
 
+        for i in range(0, self.code_iter):
+            slice = i * self.block_symbol
+            enc_msg = self.code.encode(msg[slice : slice + self.block_symbol])
+            #systematic original msg
+            fec_msg[i % slice_pkt].apend(enc_msg[: self.block_symbol])
+
+            #encode msg
+            if (self.beta - self.alfa > 1): # one src pkt is encoded into one pkt
+                fec_msg[(i % slice_pkt) + self.ratio].apend(enc_msg[self.block_symbol + 1 :])
+            else:                           # one src pkt is encoded in more than one pkt
+                for j in range (0, self.beta):
+                    enc_slice = (j + 1) * (self.block_symbol + 1)
+                    fec_msg[(i % slice_pkt) + j].apend(enc_msg[ enc_slice : enc_slice + self.block_symbol ])
         return fec_msg
 
     def decode(self, msg):
 
-        fec_msg = [0] * self.pkt_ln
-        msg_id  = msg[1]
+        #msg_id  = msg[self.ID_IDX]
+        #fec_symbols = msg[self.SYM_IDX][:]
 
         if (msg_id == 0 | msg_id == 1):
-            fec_code.dec_msg[msg_id * 512 : (msg_id + 1) * 512] = msg[3][:]
+            fec_code.dec_msg[msg_id * 512 : (msg_id + 1) * 512] = fec_symbols
             self.recv_msg += 250
 
             if (self.recv_msg == 500):
                 return fec_code.OK
-        else:
-            #fec_msg[self.pkt_ln / 2 : 2 * self.pkt_ln /2 ] =  msg[3][:]
-
-            #lost_pkt = range(self.pkt_ln / 2 , 2 * self.pkt_ln /2)
-            lost_pkt = range(1, 100)
-
-            #self.code.decode(msg, lost_pkt)
+        elif (msg_id == 2 | msg_id == 3):
+            print "ehllo"
+            #self.code.decode(fec_symbols, lost_pkt)
 
         return 1
--- wr_fec.py
+++ wr_fec.py
@@ -9,7 +9,16 @@ from channel import *
 from fec import fec_code
 
 def usage():
-    print "wr_code -i config"
+    print "wr_code -f <code> -c <config> -i <message> -l <block_len>"
+
+    print "-f \n\t Code Scheme: [1] Reed Solomon [2] LDPC [3] LT"
+    print "-i \n\t Message to encode"
+    print "-c \n\t Redundant Data Configuration"
+    print "\n\tFEC Config"
+    print "\n\t\t[1] 2 Pkt [src, coded]"
+    print "\n\t\t[2] 3 Pkt [src, coded, coded]"
+    print "\n\t\t[3] 4 Pkt [src, coded, coded, coded]"
+    print "\n\t\t[4] 4 Pkt [src/2, src/2, coded, coded]"
 
 def len_arg():
     if (len(sys.argv) == 1) :
@@ -18,7 +27,10 @@ def len_arg():
 
 def main(argv):
 
-    fec = fec_code(1)
+    code_sch = 0
+    block_ln = 0
+    fec_cfg = 0
+    msg_file = ""
 
     try:
         len_arg()
@@ -27,7 +39,7 @@ def main(argv):
         sys.exit(2)
 
     try:
-        opts, args = getopt.getopt(argv, "hi:", ["help"])
+        opts, args = getopt.getopt(argv, "hi:c:l:i:", ["help"])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -38,6 +50,17 @@ def main(argv):
             sys.exit()
         elif opt == '-i':
             msg_file = arg
+        elif opt == '-f':
+            code_sch = arg
+        elif opt == '-l':
+            block_ln = arg
+        elif opt == '-c':
+            if (arg == 0):
+                usage()
+            else:
+                fec_cfg = arg
+
+    fec = fec_code(code_sch, fec_cfg, block_ln)
 
     msg = fec.get_data(msg_file)
 
